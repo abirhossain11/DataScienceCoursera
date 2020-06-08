af18471ec341b8e38aeedd162d76dcd565e5ee9c
Adriana Santibañez <adrianasanben@gmail.com>
2020-06-08 13:19:43 -0500

    se cambio para que no tuviera que escribir todo el directorio

--- Programming assignment 1.R
+++ Programming assignment 1.R
@@ -15,7 +15,7 @@ complete <- function(directory, id =  1:332){
   # initialize nobs data frame of 2 cols of id number and complete count
   nob <- data.frame(id = id, nobs = vector("numeric", length = length(id)))
   # create a list of target id files
-  fileNames <- list.files(directory, full.names = TRUE)[id]
+  fileNames <- list.files(path = "~/Documents/LearningR/DataScienceCoursera/specdata/", full.names = TRUE)[id]
   # read each file put number of complete cases in nobs col
   for(i in seq(along.with = id)){
     dataRaw <- read.csv(fileNames[i])
