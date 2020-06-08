364b14951fcfcbc5df93e7a869b85a07b3257025
Adriana Santibañez <adrianasanben@gmail.com>
2020-06-07 21:32:07 -0500

    Cambie el comando para no tener que escribir el enorme directory siempre, solo tienes que poner pollutantmean( , "nitrate", 1:10) osea los otros dos argumentos los puedes cambiar, solo deja el espacio , y correra bien

--- Programming assignment 1.R
+++ Programming assignment 1.R
@@ -2,7 +2,7 @@
 # Calculate the mean of a pollutant across a specified list of monitors
 pollutantmean <- function(directory, pollutant, id = 1:332){
   # create a list of file names with data
-  fileNames <- list.files(directory, full.names = TRUE)[id]
+  fileNames <- list.files(path = "~/Documents/LearningR/DataScienceCoursera/specdata/" , full.names = TRUE)[id]
   # initialize dataRaw data frame to store all data in the range
   dataRaw <- data.frame() 
   for(i in seq(along.with = id)){
